069d38306fe228b5a42b22416749156434b31670
TheresaBennett <tbenne21@my.centennialcollege.ca>
2023-02-02 18:03:04 -0500

    COMP229 week2 start

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("Hello World");
+
+
+// From ES6+
+
+// Importing
+import {} from '';
+
+// Exporting 
+export const a = 1;
+
+// From CommonJS
+
+// Importing 
+
+const nameOfMypackage = require("");
+
+// Export
+
+module.exports = a = 1;
